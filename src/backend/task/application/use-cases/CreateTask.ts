5ee26442dd3cf4a86f5b7cf6d5aabe92794815ee
Edu Gonzalez Garcia <johndoe@example.com>
2023-06-02 17:33:50 +0200

    Uuid done OK

--- src/backend/task/application/use-cases/CreateTask.ts
+++ src/backend/task/application/use-cases/CreateTask.ts
@@ -1,18 +1,22 @@
+import { ID } from "../../../dependencies/IDGenerator";
 import { Task } from "../../domain/entities/Task";
 import { TaskRepository } from "../../domain/repository/TaskRepository";
 import { STATUS, Status } from "../../domain/value-object/Status";
 
 export class CreateTask {
 	private readonly taskRepository: TaskRepository;
+	private readonly taskID: string
 
 	constructor(taskRepository: TaskRepository) {
+		const idGenerator = new ID()
+		this.taskID = idGenerator.IDgenerator()
 		this.taskRepository = taskRepository;
 	}
 
 	execute(name: string, description: string, user: string): void {
 		const status = new Status(STATUS.PENDING);
 
-		const task = new Task(1, name, description, status, user);
+		const task = new Task(this.taskID, name, description, status, user);
 
 		this.taskRepository.save(task);
 	}
--- src/backend/task/domain/entities/Task.ts
+++ src/backend/task/domain/entities/Task.ts
@@ -1,23 +1,24 @@
 import { Status } from "../value-object/Status";
+import { ID } from "../../../dependencies/IDGenerator";
 
 // properties: id, task_name, task_description, status (enum: pending, on-going, done), user_task_creation
 
 export interface TaskProp {
-	readonly id: number;
+	readonly id: string;
 	taskName: string;
 	taskDescription: string;
 	status: Status;
 	userTaskCreator: string;
 }
 export class Task implements TaskProp {
-	readonly id: number;
+	readonly id: string;
 	public taskName: string;
 	public taskDescription: string;
 	public status: Status;
 	public userTaskCreator: string;
 
 	constructor(
-		id: number,
+		id: string,
 		taskName: string,
 		taskDescription: string,
 		status: Status,
