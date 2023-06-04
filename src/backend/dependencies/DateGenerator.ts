ac90366d792e1cc3c5e38fed935036e1c5a0352a
Edu Gonzalez Garcia <johndoe@example.com>
2023-06-04 23:38:58 +0200

    Date Timezone friendly

--- src/backend/dependencies/DateGenerator.ts
+++ src/backend/dependencies/DateGenerator.ts
@@ -1,30 +1,23 @@
-import { endOfMonth, endOfWeek, startOfWeek } from 'date-fns';
+import moment from "moment-timezone"
 
-function lastSundayOfTheMonth(): Date {
-    const date = new Date()
-    const lastDay = endOfMonth(date)
-    const firstDayLastWeek = startOfWeek(lastDay)
-    const lastSunday = endOfWeek(firstDayLastWeek, { weekStartsOn: 6 })
+function getTimezone(date: Date): boolean {
+    const momentDate = moment(date)
+    const isDayLightSavingTime = momentDate.isDST()
 
-    return lastSunday
+    return isDayLightSavingTime
 }
-
 export class currentDate {
     readonly date!: Date
 
     constructor() {
         const currentDate = new Date()
-        const lastSunday = lastSundayOfTheMonth()
-        if ((currentDate.getMonth() >= 3 && currentDate.getDate() > lastSunday.getDate() && currentDate.getHours() > 2)
-            &&
-            (currentDate.getMonth() <= 10 && currentDate.getDate() < lastSunday.getDate() && currentDate.getHours() < 2)) {
-                currentDate.setUTCHours(currentDate.getUTCHours() + 2)
-                this.date = currentDate
-            }
-        else {
+        const dayLightSavingTime = getTimezone(currentDate)
+        if (dayLightSavingTime) {
             currentDate.setUTCHours(currentDate.getUTCHours() + 2)
             this.date = currentDate
+        } else {
+            currentDate.setUTCHours(currentDate.getUTCHours() + 1)
+            this.date = currentDate
         }
-
     }
 }
