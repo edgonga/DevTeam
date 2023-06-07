161d90c78a9b0df7384b2060794161ffd9b6acc0
JulenJR <julenjr@hotmail.com>
2023-06-07 13:02:12 +0200

    taskMongoDBRepository -implementation-(?)

--- src/backend/task/infrastructure/repository/TaskMongoDBRepository.ts
+++ src/backend/task/infrastructure/repository/TaskMongoDBRepository.ts
@@ -1,8 +1,10 @@
 /* eslint-disable */
 import { TaskRepository } from "../../domain/repository/TaskRepository";
 import { Task } from "../../domain/entities/Task";
+import { MongoClient, Collection } from "mongodb";
 
 export class TaskMongoDBRepository implements TaskRepository {
+<<<<<<< Updated upstream
 	private readonly tasks: Array<Task | null>;
 	
 	constructor() {
@@ -12,8 +14,42 @@ export class TaskMongoDBRepository implements TaskRepository {
 	async save(): Promise<void> {
 		throw new Error("Method not implemented.");
 	}
+=======
+  private collection: Collection | null = null;
+>>>>>>> Stashed changes
 
-	async getAll(): Promise<Task[]> {
-		throw new Error("Method not implemented.");
-	}
+  constructor() {
+    this.connectToMongo().then((collection) => {
+      this.collection = collection;
+    });
+  }
+
+  private async connectToMongo(): Promise<Collection> {
+    const uri = "mongodb://localhost:27001/";
+    const dbName = "devTeam";
+    const collectionName = "tasks";
+
+    const client = await MongoClient.connect(uri);
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
+
+    return collection;
+  }
+
+  async save(task: Task): Promise<void> {
+    if (!this.collection) {
+      throw new Error("MongoDB collection is not initialized");
+    }
+
+    await this.collection.insertOne(task);
+  }
+
+  async getAll(): Promise<Array<Task | null>> {
+    if (!this.collection) {
+      throw new Error("MongoDB collection is not initialized");
+    }
+
+    const tasks = await this.collection.find().toArray();
+    return tasks;
+  }
 }
