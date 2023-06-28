4ba1060dd6225ed6d509352c30ebe3ad33c13c68
Edu Gonzalez Garcia <johndoe@example.com>
2023-06-28 18:27:49 +0200

    starting test

--- tests/dependencies/IDGenerator.test.ts
+++ tests/dependencies/IDGenerator.test.ts
@@ -1,7 +1,7 @@
-import { ID } from "../../src/backend/dependencies/IDGenerator";
+// import { ID } from "../../src/backend/dependencies/IDGenerator";
 
-test("ID returns an string of 36 characters long", () => {
-	const id = new ID().IDgenerator();
-	expect(id.length).toBe(36);
-	expect(typeof id).toBe("string");
-});
+// test("ID returns an string of 36 characters long", () => {
+// 	const id = new ID().IDgenerator();
+// 	expect(id.length).toBe(36);
+// 	expect(typeof id).toBe("string");
+// });
--- tests/user/domain/User.test.ts
+++ tests/user/domain/User.test.ts
@@ -1,14 +1,14 @@
-import { User } from "../../../src/backend/user/domain/entities/User";
+// import { User } from "../../../src/backend/user/domain/entities/User";
 
-test("a User is created correctly", () => {
-	const user = new User("Left Cap", "bob007");
-	expect(user.name).toBe("Left Cap");
-	expect(user.password).toBe("bob007");
-});
+// test("a User is created correctly", () => {
+// 	const user = new User("Left Cap", "bob007");
+// 	expect(user.name).toBe("Left Cap");
+// 	expect(user.password).toBe("bob007");
+// });
 
-test("if parameters are missing or lenght is not the minimum, an error is returned", () => {
-	expect(() => new User("", "")).toThrowError("Invalid User name");
-	expect(() => new User("Hugh", "")).toThrowError("Invalid password");
-	expect(() => new User("Left Cap", "11")).toThrowError("Invalid password");
-	expect(() => new User("w", "poppppp")).toThrowError("Invalid User name");
-});
+// test("if parameters are missing or lenght is not the minimum, an error is returned", () => {
+// 	expect(() => new User("", "")).toThrowError("Invalid User name");
+// 	expect(() => new User("Hugh", "")).toThrowError("Invalid password");
+// 	expect(() => new User("Left Cap", "11")).toThrowError("Invalid password");
+// 	expect(() => new User("w", "poppppp")).toThrowError("Invalid User name");
+// });
