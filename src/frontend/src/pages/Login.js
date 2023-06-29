6db083a7edfff737368597170116a495090222d2
JulenJR <julenjr@hotmail.com>
2023-06-29 10:13:31 +0200

    ligIn styled

--- src/frontend/src/pages/Login.js
+++ src/frontend/src/pages/Login.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 const Login = () => {
-  const [userName, setName] = useState("");
-  const [password, setPassword] = useState("");
+  const [userName, setName] = useState('');
+  const [password, setPassword] = useState('');
 
   const handleNameChange = (e) => {
     setName(e.target.value);
@@ -14,33 +14,34 @@ const Login = () => {
 
   const handleLogin = async (e) => {
     e.preventDefault();
-
+  
     try {
-      const response = await fetch("/api/login", {
-        method: "POST",
+      const response = await fetch('/', {
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify({
           userName,
           password,
         }),
       });
-
+  
       const userData = await response.json();
-
-      await fetch("/users", {
-        method: "POST",
+      await fetch('/api/users', {
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify(userData),
       });
+  
+      // TODO: Handle successful login: call to repository & push the data to db
     } catch (error) {
-      // TODO: Handle login error
-      // Display an error message to the user
+      console.log('USER not created || ERROR on : ', error);
     }
   };
+  
 
   return (
     <div>
