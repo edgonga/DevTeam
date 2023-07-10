01105a90cecc39c9375ff43a33ba472250605192
JulenJR <julenjr@hotmail.com>
2023-07-10 09:22:45 +0200

    dropdown button

--- src/frontend/src/pages/Login.js
+++ src/frontend/src/pages/Login.js
@@ -1,31 +1,35 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  const [userName, setName] = useState('');
-  const [password, setPassword] = useState('');
-  const navigate = useNavigate()
+  const [userName, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [selectedRepository, setSelectedRepository] = useState("");
+
+  const navigate = useNavigate();
 
   const handleNameChange = (e) => {
-    
     setName(e.target.value);
   };
 
   const handlePasswordChange = (e) => {
-    
     setPassword(e.target.value);
+  };
+
+  const handleRepositoryChange = (e) => {
+    setSelectedRepository(e.target.value);
   };
 
   const handleLogin = async (e) => {
     e.preventDefault();
-  
+
     try {
       await new Promise((resolve) => setTimeout(resolve, 2000));
 
-      await fetch('http://localhost:8000/user', {
-        method: 'POST',
+      await fetch("http://localhost:8000/user", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           userName,
@@ -34,35 +38,41 @@ const Login = () => {
       });
       console.log(`User: ${userName} created`);
 
-      navigate('/home')
-  
+      navigate("/home");
+
       // TODO: Handle successful login: call to repository & push the data to db
     } catch (error) {
-      console.log('USER not created || ERROR on : ', error);
+      console.log("USER not created || ERROR on : ", error);
     }
   };
-  
-// Submit NO logic
+
+  // Submit NO logic
   return (
     <>
-    <div>  
-      <h2>Login</h2>
-      <form onSubmit={handleLogin}>
-        <input
-          type="name"
-          placeholder="name"
-          value={userName}
-          onChange={handleNameChange}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={handlePasswordChange}
-        />
-        <button type="submit">Login</button>
-      </form>
-    </div>
+      <div>
+        <h2>Login</h2>
+        <form onSubmit={handleLogin}>
+          <input
+            type="name"
+            placeholder="name"
+            value={userName}
+            onChange={handleNameChange}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+          <button type="submit">Login</button>
+          <select value={selectedRepository} onChange={handleRepositoryChange}>
+            <option value="inMemory">In Memory</option>
+            <option value="json">JSON</option>
+            <option value="mongo">MongoDB</option>
+            <option value="mysql">MySQL</option>
+          </select>
+        </form>
+      </div>
     </>
   );
 };
