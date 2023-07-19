6517fca70a2b00da269b77defb72ed9f8d5e4588
blablabla <blabla>
2023-07-19 11:45:58 +0200

    done

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -3,7 +3,6 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from './pages/Login'
 import Home from "./pages/Home";
-import Repo from "./pages/Repo";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -13,7 +12,6 @@ function App() {
       <Navbar/>
         <Routes>
           <Route path="/" element={<Login/>} />
-          <Route path="/repo" element={<Repo/>} />
           <Route path="/home" element={<Home/>} />
         </Routes>
       </div>
